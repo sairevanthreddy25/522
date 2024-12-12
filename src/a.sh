935e059ac629d6db5fd1d399f6fa9232d40012a3
revanthr <revanthr@localhost>
2024-12-12 00:50:47 -0600

    Reinitialize repository without large files

---
+++ src/a.sh
@@ -0,0 +1,3 @@
+make clean && make
+rm -rf ../results/Inceptionv3/1024-example/
+./sim configs/example.config
---
+++ src/b.sh
@@ -0,0 +1,2 @@
+rm -rf ../results/Inceptionv3/1024-example/
+./sim configs/example.config
